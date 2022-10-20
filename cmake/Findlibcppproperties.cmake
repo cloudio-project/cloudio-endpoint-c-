db2f27946fbbf8bd8a63d0d5d75a0bb7c5254514
Lucas Bonvin <lucasbonvin@hotmail.com>
2022-10-20 13:26:36 +0200

    First commit of cmake clion project

---
+++ cmake/Findlibcppproperties.cmake
@@ -0,0 +1,10 @@
+set(FIND_CPP_PROPERTIES_PATHS /home/lucas/Documents/libraries/cpp-properties)
+
+find_path(CPP_PROPERTIES_INCLUDE_DIR Properties.h Properties.h
+        PATH_SUFFIXES include
+        PATHS ${FIND_CPP_PROPERTIES_PATHS})
+
+find_library(CPP_PROPERTIES_LIBRARY
+        NAMES cppproperties
+        PATH_SUFFIXES lib
+        PATHS ${FIND_CPP_PROPERTIES_PATHS})
