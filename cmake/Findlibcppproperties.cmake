1b0670913f35d611cda8b716c94b16c39a97f68b
Lucas Bonvin <lucasbonvin@hotmail.com>
2022-11-03 10:35:56 +0100

    cleaned cmake files

--- cmake/Findlibcppproperties.cmake
+++ cmake/Findlibcppproperties.cmake
@@ -1,10 +1,12 @@
-set(FIND_CPP_PROPERTIES_PATHS /home/lucas/Documents/libraries/cpp-properties)
+set(FIND_CPP_PROPERTIES_PATHS
+        ${CMAKE_CURRENT_SOURCE_DIR}/../cpp-properties
+        )
 
-find_path(CPP_PROPERTIES_INCLUDE_DIR Properties.h Properties.h
+find_path(CPP_PROPERTIES_INCLUDE_DIR Properties.h
         PATH_SUFFIXES include
         PATHS ${FIND_CPP_PROPERTIES_PATHS})
 
 find_library(CPP_PROPERTIES_LIBRARY
-        NAMES cppproperties
+        NAMES libcppproperties
         PATH_SUFFIXES lib
         PATHS ${FIND_CPP_PROPERTIES_PATHS})
